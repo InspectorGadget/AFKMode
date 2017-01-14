13ca590ae5a2b520b7ee4ea2548cd90a0c131a57
InspectorGadget <igadget28@gmail.com>
2017-01-14 16:44:16 +0800

    Revamped AFKMode

--- src/RTG/AFK/AFK.php
+++ src/RTG/AFK/AFK.php
@@ -14,8 +14,6 @@ use pocketmine\event\Listener;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
-use pocketmine\utils\Config;
-
 use pocketmine\event\Cancellable;
 
 use pocketmine\Server;
@@ -23,74 +21,80 @@ use pocketmine\Player;
 
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+
+use pocketmine\utils\TextFormat as TF;
 
 class AFK extends PluginBase implements Listener {
 	
 	public function onEnable() {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->saveResource("afk.txt");
-		$this->saveResource("config.yml");
-		$this->a = new Config($this->getDataFolder() . "afk.txt");
-		$this->c = new Config($this->getDataFolder() . "config.yml");
+		$this->enabled = array();
 		$this->getLogger()->warning("
 		* Loading AFKMode...
 		* Checking Version
-		* 1.0.0 Passed!
+		* 1.0.1 Passed!
 		* AFKMode has been loaded!
 		");
 	}
 	
-	public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
+	public function onCommand(CommandSender $sender, Command $cmd, $label, array $param) {
 		switch(strtolower($cmd->getName())) {
 			
-			case "afkmode":
-				if($sender instanceof Player) {
-					if($sender->hasPermission("afkmode.toggle")) {
-						$n = $sender->getName();
-						
-						if($this->a->get($n) === false) {
-							$this->a->set($n);
-							$this->a->save();
-							$this->getServer()->broadcastMessage("§d[Server] [] $n is now AFK!");
-						}
-						else {
-							$n = $sender->getName();
-							if($this->a->get($n) === true) {
-								$this->a->remove($n);
-								$this->a->save();
-								$this->getServer()->broadcastMessage("§d[Server] [] $n is no longer AFK!");
-									if($this->c->get("bc") === true) {
-										$this->getServer()->broadcastMessage("§d[Server] Thank you for using AFK by IG!");
-									}
-							}
-						}
+			case "afk":
+				if($sender->hasPermission("afk.toggle") or $sender->isOp()) {
+					
+					if(isset($this->enabled[strtolower($sender->getName())])) {
+						unset($this->enabled[strtolower($sender->getName())]);
+						$sender->sendMessage("[God]" . TF::RED . " You have left AFKMode!");
 					}
 					else {
-						$sender->sendMessage("§cYou have no permission to use this command!");
+						$this->enabled[strtolower($sender->getName())] = strtolower($sender->getName());
+						$sender->sendMessage("[God]" . TF::GREEN . " You have joined AFKMode!");
 					}
+					
 				}
 				else {
-					$sender->sendMessage("Only in-game!");
+					$sender->sendMessage(TF::RED . "You have no permission to use this command!");
 				}
 				return true;
 			break;
+			
 		}
 	}
-	
+			
 	public function onMove(PlayerMoveEvent $e) {
 		$p = $e->getPlayer();
-		if($this->a->get($p->getName()) === true) {
-			$p->sendPopup("§cPlease do /afkmode to leave AFK Mode!");
-			$e->setCancelled();
-		}
+			
+			if(isset($this->enabled[strtolower($p->getName())])) {
+				$p->sendPopup(TF::RED . "Please leave AFKMode to walk!");
+				$e->setCancelled(true);
+			}
+		
 	}
-	
+			
 	public function onChat(PlayerChatEvent $e) {
 		$p = $e->getPlayer();
-		if($this->a->get($p->getName()) === true) {
-			$p->sendMessage("§cPlease do /afkmode to leave AFK Mode!");
-			$e->setCancelled();
-		}
+		
+			if(isset($this->enabled[strtolower($p->getName())])) {
+				$p->sendMessage(TF::RED . "Please leave AFKMode to chat!");
+				$p->sendMessage(TF::GREEN . "You can leave AFKMode by typing /afk again!");
+				$e->setCancelled(true);
+			}
+	
+	}
+	
+	public function onJoin(PlayerJoinEvent $e) {
+		$p = $e->getPlayer();
+		
+			if(isset($this->enabled[strtolower($p->getName())])) {
+				unset($this->enabled[strtolower($p->getName())]);
+				$sender->sendMessage(TF::RED . "Your mode has been reset!, you are no longer AFK!");
+			}
+			
+	}
+		
+	public function onDisable() {
 	}
 	
 }
